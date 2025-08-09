9a8e5554c1e90dc745614b528fe88813836953d6
William Peters <william.john.peters@gmail.com>
2025-08-08 22:27:44 -0400

    Five fold

--- app.py
+++ app.py
@@ -277,8 +277,8 @@ with tab4:
     st.image(Evaluation_Metrics)
 
     st.subheader("5_Fold Accuracy")
-    '5_Fold' = Image.open("5_fold Accuracy.png")
-    st.image('5_Fold')
+    Five_Fold = Image.open("5_fold Accuracy.png")
+    st.image(Five_Fold)
 
     st.subheader("Box_Plot")
     Box_Plot = Image.open("Box_Plot 5_fold.png")
